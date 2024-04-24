6b74b3a2a82693c5cfc01411dc65264eec3445a0
amsu21 <aswb4@hotmail.com>
2024-04-23 20:05:25 -0400

    Added controller function

--- Summative-1-Warner-Amsu/src/main/java/com/company/Summative1WarnerAmsu/controller/ShoeController.java
+++ Summative-1-Warner-Amsu/src/main/java/com/company/Summative1WarnerAmsu/controller/ShoeController.java
@@ -10,4 +10,26 @@ import java.util.Random;
 
 @RestController
 public class ShoeController {
+    private final List<Shoe> shoeList;
+//    private static int idCounter = 1;
+
+    public ShoeController() {
+        shoeList = new ArrayList<>();
+
+        // SHOES
+        shoeList.add(new Shoe("Nike", "Air Max 90", 1990));
+        shoeList.add(new Shoe("Jordan", "Air Jordan 3", 1988));
+        shoeList.add(new Shoe("Asics", "Gel Kayano 14", 2008));
+        shoeList.add(new Shoe("New Balance", "990v3", 2012));
+        shoeList.add(new Shoe("Nike", "Mac Attack", 1984));
+    }
+
+    // GET ALL SHOES
+    @RequestMapping(value="/shoe", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public Shoe getShoe() {
+        Random random = new Random();
+        return shoeList.get(random.nextInt(5));
+    }
+
 }
